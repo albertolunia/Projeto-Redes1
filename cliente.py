0d16e23348b9bddbe948da8aacc97ace52d15ae8
Alberto Henrique <albertolunia@gmail.com>
2023-12-03 17:48:45 -0300

    Projeto final

--- cliente.py
+++ cliente.py
@@ -6,36 +6,49 @@ PORT = 12345
 
 # Função para solicitar e imprimir a lista de itens
 def get_item_list(client_socket):
-    client_socket.sendall("LIST_ITEMS_REQUEST".encode())
-    data = client_socket.recv(1024).decode()
-    print(data)
+    try:
+        client_socket.sendall("LIST_ITEMS_REQUEST".encode())
+        data = client_socket.recv(1024).decode()
+        print(data)
+    except ConnectionError as e:
+        print(f"Erro de conexão: {e}")
 
 # Função para enviar um lance
-def place_bid(client_socket, item, new_bid):
-    message = f"BID {item} {new_bid}"
-    client_socket.sendall(message.encode())
-    response = client_socket.recv(1024).decode()
-    print(response)
+def place_bid(client_socket, name, item, new_bid):
+    try:
+        message = f"BID {name} {item} {new_bid}"
+        client_socket.sendall(message.encode())
+        response = client_socket.recv(1024).decode()
+        print(response)
+    except (ValueError, ConnectionError) as e:
+        print(f"Erro durante o lance: {e}")
 
 # Configuração do socket
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
-    client_socket.connect((HOST, PORT))
+    try:
+        client_socket.connect((HOST, PORT))
 
-    while True:
-        print("\nEscolha uma opção:")
-        print("1. Ver lista de itens")
-        print("2. Fazer um lance")
-        print("3. Sair")
+        while True:
+            print("\nEscolha uma opção:")
+            print("1. Ver lista de itens")
+            print("2. Fazer um lance")
+            print("3. Sair")
 
-        choice = input("Opção: ")
+            choice = input("Opção: ")
 
-        if choice == "1":
-            get_item_list(client_socket)
-        elif choice == "2":
-            item = input("Digite o nome do item: ")
-            bid = int(input("Digite o valor do lance: "))
-            place_bid(client_socket, item, bid)
-        elif choice == "3":
-            break
-        else:
-            print("Opção inválida. Tente novamente.")
+            if choice == "1":
+                get_item_list(client_socket)
+            elif choice == "2":
+                try:
+                    name = input("Digite seu nome: ")
+                    item = input("Digite o ID do item: ")
+                    bid = int(input("Digite o valor do lance: "))
+                    place_bid(client_socket, name, item, bid)
+                except ValueError:
+                    print("Valor do lance inválido. Tente novamente.")
+            elif choice == "3":
+                break
+            else:
+                print("Opção inválida. Tente novamente.")
+    except ConnectionError as e:
+        print(f"Erro de conexão: {e}")
--- servidor.py
+++ servidor.py
@@ -1,26 +1,34 @@
 import socket
+import time
 
 # Função para lidar com lances
-def process_bid(item, new_bid):
-    global items
-    current_bid = items.get(item, 0)
-    if new_bid > current_bid:
-        items[item] = new_bid
-        return "BID_ACCEPTED"
+def process_bid(name, item_id, new_bid):
+    global items, highest_bids
+    item_names = list(items.keys())
+    if 1 <= item_id <= len(item_names):
+        item = item_names[item_id - 1]
+        current_bid = items.get(item, 0)
+        if new_bid > current_bid:
+            items[item] = new_bid
+            highest_bids[item] = (name, new_bid)  # Atualiza o maior lance para este item
+            return f"\n{name} fez o lance com sucesso! ✔️ Lance Aceito"
+        else:
+            return f"\n{name}, seu lance foi recusado. ❌ Valor abaixo do lance atual"
     else:
-        return "BID_REJECTED"
+        return "\n❗ ID do item inválido"
 
 # Função para lidar com solicitação de lista de itens
 def get_item_list():
-    item_list = ";".join([f"{item},{value}" for item, value in items.items()])
-    return f"ITEM_LIST {item_list}"
+    item_list = "\n".join([f"ID: {index} - Item: {item} - Valor: R${value}" for index, (item, value) in enumerate(items.items(), start=1)])
+    return f"===== Leilão da Polícia Federal =====\n{item_list}\n======================"
 
 # Configuração do servidor
 HOST = '127.0.0.1'
 PORT = 12345
 
 # Dados iniciais
-items = {'item1': 100, 'item2': 150}
+items = {'iPhone 13 Pro (128GB)': 4999, 'Laptop Dell XPS 15': 6499, 'Samsung Galaxy Tab S7+': 2299, 'NVIDIA GeForce RTX 3080': 1999}
+highest_bids = {item: ("", 0) for item in items}  # Inicializa os maiores lances como vazio para cada item
 
 # Configuração do socket
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
@@ -29,22 +37,37 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
 
     print(f"Servidor ouvindo em {HOST}:{PORT}")
 
-    conn, addr = server_socket.accept()
-    with conn:
-        print(f"Conectado por {addr}")
-        while True:
-            data = conn.recv(1024).decode()
-            if not data:
-                break
+    # Tempo limite para o leilão (exemplo de 5 minutos)
+    tempo_limite = time.time() + 300  # 5 minutos em segundos
 
-            # Processa a mensagem do cliente
-            if data == "LIST_ITEMS_REQUEST":
-                response = get_item_list()
-            elif data.startswith("BID"):
-                _, item, new_bid = data.split()
-                response = process_bid(item, int(new_bid))
-            else:
-                response = "INVALID_REQUEST"
+    while time.time() < tempo_limite:
+        conn, addr = server_socket.accept()
+        with conn:
+            print(f"Conectado por {addr}")
+            while True:
+                data = conn.recv(1024).decode()
+                if not data:
+                    break
 
-            # Envia resposta de volta ao cliente
-            conn.sendall(response.encode())
+                # Processa a mensagem do cliente
+                if data == "LIST_ITEMS_REQUEST":
+                    response = get_item_list()
+                elif data.startswith("BID"):
+                    try:
+                        _, name, item_id, new_bid = data.split()
+                        response = process_bid(name, int(item_id), int(new_bid))
+                    except ValueError:
+                        response = "\n❗ Formato de lance inválido"
+                else:
+                    response = "\n❗ INVALID REQUEST"
+
+                # Envia resposta de volta ao cliente
+                conn.sendall(response.encode())
+
+    # Após atingir o tempo limite
+    print("\nLeilão encerrado. Resultados finais:")
+    for item, (name, bid) in highest_bids.items():
+        if name != "":
+            print(f"Item: {item} - Maior Lance por {name}: R${bid}")
+        else:
+            print(f"Item: {item} - Sem lances")
